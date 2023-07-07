0d292599f7b54bdde39a8ee06f2d57e2c2158a3c
srinu-11 <srinivasu@chasealpha.in>
2023-07-07 07:15:58 +0530

    test commit

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    print("test");
     return MultiBlocProvider(
       providers: [
         BlocProvider(create: (_) => HomeBloc()),
